b602067f3e550915ac9860343a83f595441a54b0
kanechandev <kanes.chan@gmail.com>
2025-05-21 08:31:52 -0300

    Adaptado a classe Produto para relacionamento n:n

--- src/main/java/com/kanechan/restaurante/model/Produto.java
+++ src/main/java/com/kanechan/restaurante/model/Produto.java
@@ -1,18 +1,28 @@
 package com.kanechan.restaurante.model;
 
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "produto")
-public class Produto {
+public class Produto implements Serializable{
+	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,25 +31,103 @@ public class Produto {
 	private String nome;
 	private Double preco;
 	
-	@ManyToOne
-	@JoinColumn(name = "categoria_id")
-	private Categoria categoria;
+	@Column(columnDefinition = "TEXT")
+	private String descricao;
+	
+	private String imgUrl;
+	
+	@CreationTimestamp
+	@Column(name = "created_at", columnDefinition = "TIMESTAMP", nullable = false)
+	private LocalDateTime createdAt;
+	
+	@UpdateTimestamp
+	@Column(name = "updated_at", columnDefinition = "TIMESTAMP")
+	private LocalDateTime updatedAt;
+	
+	@ManyToMany
+	@JoinTable(name = "produto_categoria",
+		joinColumns = @JoinColumn(name = "produto_id"),
+		inverseJoinColumns = @JoinColumn(name = "categoria_id")
+			)
+	private Set<Categoria> categorias = new HashSet<>();
 	
 	public Produto() {
 		super();
 	}
 
-	public Produto(Long id, String nome, Double preco, Categoria categoria) {
+	public Produto(Long id, String nome, Double preco, String descricao, String imgUrl, Set<Categoria> categorias) {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
-		this.categoria = categoria;
+		this.descricao = descricao;
+		this.imgUrl = imgUrl;
+		this.categorias = categorias;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Set<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void adicionarCategoria(Categoria categoria) {
+		categorias.add(categoria);
+	}
+	
+	public void removerCategoria(Categoria categoria) {
+		categorias.remove(categoria);
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(categoria, id, nome, preco);
+		return Objects.hash(id);
 	}
 
 	@Override
@@ -51,8 +139,7 @@ public class Produto {
 		if (getClass() != obj.getClass())
 			return false;
 		Produto other = (Produto) obj;
-		return Objects.equals(categoria, other.categoria) && Objects.equals(id, other.id)
-				&& Objects.equals(nome, other.nome) && Objects.equals(preco, other.preco);
+		return Objects.equals(id, other.id);
 	}
 	
 }
